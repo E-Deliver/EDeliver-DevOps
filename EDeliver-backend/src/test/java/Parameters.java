3092c6bde3b71f9d9dbc6825ab075ce3391cfcc3
Khadija El Madani <khadijaelmadani24@gmail.com>
2024-10-28 01:01:11 +0100

    New commit

--- EDeliver-backend/src/test/java/Parameters.java
+++
@@ -1,7 +0,0 @@
-public class Parameters {
-    //***********LoginURLS**************
-    public static String UrlDashLivreurPage="http://localhost:4200/dashboard-livreur";
-    public static String UrlDashClientPage="http://localhost:4200/dashboard-client";
-    public static String UrlDashAdminPage="http://localhost:4200/dashboard-administrateur";
-    //*****************
-}
